cd6c301c628492afb4ffddf34c3e9d37a501b3dc
yangfan1 <yangfan1@sensetime.com>
2020-09-16 23:36:05 +0800

    add 1.2

--- 1.2_一个简单的二维向量类/1.2_一个简单的二维向量类.py
+++ 1.2_一个简单的二维向量类/1.2_一个简单的二维向量类.py
@@ -20,6 +20,26 @@ class Vector:
     def __bool__(self):
         return bool(abs(self))
 
-    
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
 
 
+def main():
+    v1 = Vector(2, 4)
+    v2 = Vector(2, 1)
+    print(v1 + v2)
+
+    v = Vector(3, 4)
+    print(abs(v))
+
+    print(v * 3)
+
+    print(abs(v * 3))
+
+if __name__ == '__main__':
+    main()
